e31ac0de6530e9fc16c766785c5f8cc1eca46391
mateustxr <mateustxr1@gmail.com>
2022-04-20 17:00:03 -0300

    exercicios vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import BlocoPrincipal from "./components/BlocoPrincipal.vue";
+export default {
+  components: { MenuLateral, BlocoPrincipal },
+};
+</script>
+<template>
+  <MenuLateral />
+  <header id="header">Cabeçalho</header>
+  <main id="content">Principal</main>
+  <footer id="footer">Rodapé</footer>
+  <BlocoPrincipal />
+</template>
+<style></style>
